a3d50106404d72a53f9fe94c64deb884a85cf214
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-06-11 16:29:59 +0530

    Data Received by Backend As Object Using POST Rewuest

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
+import { Box, Button, Container, Flex, Text } from '@chakra-ui/react';
+import { useState } from 'react';
 import { QrReader } from 'react-qr-reader';
-import { Container, Box, Text, Button, Flex } from '@chakra-ui/react'
 
-function App(){
+function App() {
   const [data, setData] = useState('No result');
   const [screen, setScreen] = useState(false);
-  const [num, setNum] = useState(0);
-  const dataList = ["sahil"];
-  const [lists, setLists] = useState(dataList);
+  // const [num, setNum] = useState(0);
+  const scannedList = [""];
+  const [lists, setLists] = useState(scannedList);
   const startCamera = () => {
-    if(!screen){
+    if (!screen) {
       setScreen(true);
     }
   }
   const stopCamera = () => {
-    if(screen){
+    if (screen) {
       setScreen(false);
-    }   
+    }
   }
 
   const clearList = () => {
@@ -28,62 +28,74 @@ function App(){
   //   }
   // }
 
-  
+
 
   return (
     <>
-    <Text fontSize='4xl' color='#4FD1C5' textAlign={'center'}>QR Code Scanner</Text>
-    <Flex maxW='95%'>
-      
-    <Box width={'30%'} p={4} textAlign="center">
-    
-    {screen && <Container>
-      <QrReader
-        onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.text);
-            let n =0;
-            if (data != null){
-              if(dataList.includes(result?.text)){
-                alert("User already scanned");
-              }
-              else{
-                n = dataList.push(result?.text);
-                setLists(dataList);
-                setNum(n);
-              }
-            }
-            
-          }
+      <Text fontSize='4xl' color='#4FD1C5' textAlign={'center'}>QR Code Scanner</Text>
+      <Flex maxW='95%'>
 
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-      />
-    </Container> }
-    <Flex color='white'>
-      <Button colorScheme='blue' mr={2} onClick={() => startCamera()}>Start Camera</Button>
-      <Button colorScheme='blue' onClick={() => stopCamera()}>Stop Camera</Button>
-      
-    </Flex>
-    {/* <Button colorScheme='blue' onClick={() => clearList()}>Clear List</Button> */}
-    
-    </Box>
-      <Box width={'70%'}>
-      <Text fontSize='4xl' color='#000000' textAlign={'center'}>Scanned Detail</Text>
-      <Box>
-        <ol>
-          {lists && lists.map((el) => {{
-            return <li>{el}</li>
-          }})}
-          {/* <li>{lists}</li> */}
-        </ol>
-        {/* <p color="white">{data}</p> */}
-      </Box>
-      </Box>
-   </Flex>
-      
+        <Box width={'30%'} p={4} textAlign="center">
+
+          {screen && <Container>
+            <QrReader
+              onResult={(result, error) => {
+                console.log(result);
+                if (result) {
+
+                  // setData(result?.text);
+
+                  let n = 0;
+                  if (result?.text != null) {
+
+                    if (scannedList.includes(result?.text)) {
+                      alert("User already scanned");
+                      setScreen(false)
+                    }
+
+                    else {
+                      scannedList.push(result?.text);
+                      setLists(scannedList);
+                      fetch(`http://localhost:8080/insert/data`, {
+                        method: 'POST',
+                        body: lists
+                      })
+                      setScreen(false)
+                      // setNum(n);
+                    }
+                  }
+
+                }
+                if (error) {
+                  console.info(error);
+                }
+              }}
+            />
+          </Container>}
+          <Flex color='white'>
+            <Button colorScheme='blue' mr={2} onClick={() => startCamera()}>Start Camera</Button>
+            <Button colorScheme='blue' onClick={() => stopCamera()}>Stop Camera</Button>
+
+          </Flex>
+          {/* <Button colorScheme='blue' onClick={() => clearList()}>Clear List</Button> */}
+
+        </Box>
+        <Box width={'70%'}>
+          <Text fontSize='4xl' color='#000000' textAlign={'center'}>Scanned Detail</Text>
+          <Box>
+            <ol>
+              {lists && lists.map((el) => {
+                {
+                  return <li>{el}</li>
+                }
+              })}
+              {/* <li>{lists}</li> */}
+            </ol>
+            {/* <p color="white">{data}</p> */}
+          </Box>
+        </Box>
+      </Flex>
+
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,21 +1,33 @@
 const express = require('express');
-const app =express();
+const app = express();
+const bodyParser = require('body-parser');
 const mysql = require("mysql");
 
-const db= mysql.createPool({
+const db = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "password",
     database: "Kuchbhi",
 });
 
-
-
-app.get('/',(req,res)=>{
+app.use(bodyParser.json({ type: 'application/json' }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.get('/', (req, res) => {
     const sqlInsert = "INSERT INTO table_name (column1, column2) VALUES ('value1', 'value2');";
-    
-})
+    res.status(200).send({ name: "VIVEK" })
 
-app.listen(3000, ()=>{
-    console.log("hua hua");
+})
+app.post("/insert/:data", (req, res) => {
+    // console.log(req.params.name);
+    // const data=req.body.firstname;
+    // const data=req.body.lastname;
+    // const data=req.body.enrollmentno;
+    // const data=req.body.department;
+    // const data=req.body.college;
+    // res.status(200).send(req.body)
+    // console.log(req.body);
+    res.send(req.body)
+})
+app.listen(8080, () => {
+    // console.log("hua hua");
 })
